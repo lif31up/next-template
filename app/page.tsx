e89dc931bd800f262390321c14609d50f6ecbde0
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-02-06 10:58:01 +0900

    new: 파일 구조를 정의하고 호환성을 규정했습니다.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import FirstBlock from "@/components/feature/FirstBlock";
-
 export default function Home() {
   return (
-    <main className="w-full h-screen grid justify-items-center items-center">
-      <FirstBlock/>
+    <main>
+
     </main>
   );
 }
