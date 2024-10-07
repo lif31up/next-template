6747fac29c5b86c15d5dd0386ebe9518ddfdf6f8
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-10-08 03:28:26 +0900

    Update HorizGallery.tsx

--- component/common/HorizGallery.tsx
+++ component/common/HorizGallery.tsx
@@ -1,10 +1,42 @@
 import DefaultProps from "@/utils/DefaultProps";
+import TailProperties, { TailClassName } from "@/styles/TailProperties";
 
 export default HorizGallery;
 
-function HorizGallery({children, styl}: DefaultProps<never>) {
+interface HorizGalleryProps extends DefaultProps<never> {
+  children: JSX.Element[];
+  tailnameOfFrame: string;
+} // HorizGalleryProps
+function HorizGallery({
+  children,
+  tailnameOfFrame,
+  className,
+}: HorizGalleryProps) {
+  const frames: JSX.Element[] = [];
+  children.forEach((child: JSX.Element, index: number): void => {
+    frames.push(
+      <Frame key={index} className={tailnameOfFrame}>
+        {child}
+      </Frame>
+    ); // push
+  }); //forEach
 
-	return <section style={}>
-		{children}
-	</section>
-} // HroizGallery
+  const tailname: TailProperties = {
+    box: "w-full h-full",
+    layout: "overflow-hidden snap-mandatory flex items-cennter",
+  }; // tailname
+  return (
+    <section className={`${TailClassName(tailname)} ${className}`}>
+      {frames}
+    </section>
+  ); // return
+} // HorizGallery
+
+function Frame({ children, className }: DefaultProps<never>) {
+  const tailname: TailProperties = {
+    box: "w-fit h-fit",
+  }; // tailname
+  return (
+    <div className={`${TailClassName(tailname)} ${className}`}>{children}</div>
+  ); // return
+} // Frame
